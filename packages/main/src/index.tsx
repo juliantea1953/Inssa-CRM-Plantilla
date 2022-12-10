dd2a2f59c75748b12d96718f2c7b78903dc6c573
juliantea1953 <davidtea1953@gmail.com>
2022-12-10 07:24:00 -0500

    cambio de rutas de el favicom y logo

--- packages/main/src/index.tsx
+++ packages/main/src/index.tsx
@@ -5,9 +5,13 @@ import ThemeProvider from "./redux/providers/theme-provider";
 import PersistProvider from "./redux/providers/persist-provider";
 import { store } from "./redux/store";
 import App from "../src/App";
+import React from "react";
+import ReactDom from "react-dom/client";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+/* const root = ReactDom.createRoot(document.getElementById("root")); */
 
 root.render(
     <Provider store={store}>
